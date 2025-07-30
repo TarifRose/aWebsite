ec3d8f5c669343ab1598388860c5d3bf900ac33c
TarifRose <47014384+TarifRose@users.noreply.github.com>
2025-07-30 02:04:08 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,28 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-
-users={
-    "admin":"tos",
-    "usr": "usrprofile"
+users = {
+    "tos": {"password": "123", "redirect": "mineui"},
+    "usr": {"password": "usrprofile", "redirect": "mineui"}
 }
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
 
-    #input validation
     if not data or 'username' not in data or 'password' not in data:
-        return jsonify({'message': 'Incorrect Credentials'}), 400 
+        return jsonify({'message': 'Incorrect Credentials'}), 400
+
     username = data['username']
     password = data['password']
 
-    #auth
-    if username in users and users[username] ==password:
-        return jsonify({'message': 'Login Successful'}), 200 
+    if username in users and users[username]['password'] == password:
+        redirect_page = users[username]['redirect']
+        return redirect(url_for(redirect_page))
     else:
-        return jsonify({'message':'invalid user or password'}), 401 
-    
-    if __name__ =='__main__':
-        app.run(debug=True)
+        return jsonify({'message': 'Invalid user or password'}), 401
+
+@app.route('/mineui')
+def mineui():
+    return render_template('mineui.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
